e0d4142f3c496340f2c1e6d813c9952e2011f31a
gcastrolinares <Gerry.7914@gmail.com>
2026-03-04 11:40:23 +0100

    Update 2.Cell_analysis.py

--- CD19-ZAP70_analysis/2.Cell_analysis.py
+++ CD19-ZAP70_analysis/2.Cell_analysis.py
@@ -1,11 +1,4 @@
 #%%
-
-"""
-Created on Fri Aug 22 09:54:03 2025
-
-@author: castrolinares
-"""
-
 from postSPIT import plotting_classes as plc
 import matplotlib.pyplot as plt
 import pandas as pd 
@@ -14,7 +7,7 @@ from natsort import natsorted
 from tensorflow.keras.models import load_model
 
 #%%
-#load the object to run the analysis
+#load the object to run the analysis for all FOV within a folder. Ch0 is CD19, Ch1 is ZAP70.  
 data = plc.Dataset_combined_analysis(r'D:\Data\Chi_data\20250801_filtered', ch0_hint='638nm', ch1_hint='488nm')
 #Runs combine_spots_clusters. This will:
 # - run cluster detection anad analysis if it has not been done. 
@@ -28,4 +21,4 @@ data.retrack()
 data.recoloc_tracks()
 # Load and use pre-trained model to analyze the maturation state of the immune synapses per frame. 
 model = load_model(r'C:\Users\castrolinares\Data analysis\SPIT_G\ML_mature\V2\DL\MobileNetV2_expandedDataSet_V8.h5')
-data.predict_maturation(model, save_plot = True, ch = 'ch1', )
+data.predict_maturation(model, save_plot = True, ch = 'ch1')
